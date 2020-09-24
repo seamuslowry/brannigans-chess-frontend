575db0c6fa3234e6739010f2a982752a3bb0d937
Seamus Lowry <spl1owry3@gmail.com>
2020-09-24 09:22:23 -0400

    add config for different environment variables

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+interface Configuration {
+    serviceUrl: string;
+}
+
+const dev: Configuration = {
+    serviceUrl: 'https://backend-dev.branniganschess.com'
+}
+
+const prod: Configuration = {
+    serviceUrl: 'https://backend.branniganschess.com'
+}
+
+const local: Configuration = {
+    serviceUrl: 'http://localhost:8080'
+}
+
+let config: Configuration;
+
+switch (process.env.REACT_APP_ENV) {
+    case "production":
+        config = prod;
+        break;
+    case "development":
+        config = dev;
+        break;
+    default:
+        config = local;
+        break;
+}
+
+
+export default config;
