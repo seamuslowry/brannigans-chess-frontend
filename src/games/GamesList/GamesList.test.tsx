e6838cee9500912e742a9e026afbf37dbe357bfd
Seamus Lowry <spl1owry3@gmail.com>
2020-09-26 17:05:47 -0400

    render games better

--- src/games/GamesList/GamesList.test.tsx
+++ src/games/GamesList/GamesList.test.tsx
@@ -7,8 +7,8 @@ import { Game } from '../../services/ChessService';
 import config from '../../config';
 
 const gamesResponse: Game[] = [
-  { id: 1, uuid: 'uuid1' },
-  { id: 2, uuid: 'uuid2' }
+  { id: 1, uuid: 'uuid1', whitePlayer: null, blackPlayer: null, winner: null },
+  { id: 2, uuid: 'uuid2', whitePlayer: null, blackPlayer: null, winner: null }
 ];
 
 const server = setupServer(
@@ -24,7 +24,7 @@ afterAll(() => server.close());
 test('renders games as a list', async () => {
   render(<GamesList />);
 
-  const gameNodes = await waitFor(() => screen.getAllByText(/Game Id/i));
+  const gameNodes = await waitFor(() => screen.getAllByTestId('game-list-item'));
 
   expect(gameNodes).toHaveLength(2);
 });
@@ -41,4 +41,35 @@ test('handles error when getting lists', async () => {
   const error = await waitFor(() => screen.getByText(/Could not load game/i));
 
   expect(error).toBeInTheDocument();
+});
+
+test('allows users to join as white or black when available', async () => {
+  render(<GamesList />);
+
+  const joinNodes = await waitFor(() => screen.getAllByTitle(/join as/i));
+
+  expect(joinNodes).toHaveLength(gamesResponse.length * 2);
+});
+
+test('disallows joining when both colors are unavailable', async () => {
+  server.use(
+    rest.get(`${config.serviceUrl}/games?active=true`, (req, res, ctx) => {
+      return res(
+        ctx.json<Game[]>([
+          {
+            uuid: 'uuid filled',
+            id: 3,
+            whitePlayer: { username: 'test', email: 'test', id: 1 },
+            blackPlayer: { username: 'test2', email: 'test2', id: 2 },
+            winner: null
+          }
+        ])
+      );
+    })
+  );
+  render(<GamesList />);
+
+  const joinNodes = await waitFor(() => screen.getAllByTitle(/is playing/i));
+
+  expect(joinNodes).toHaveLength(2);
 });
--- src/games/GamesList/GamesList.tsx
+++ src/games/GamesList/GamesList.tsx
@@ -1,18 +1,74 @@
 import React from 'react';
-import { CircularProgress, Typography } from '@material-ui/core';
+import {
+  CircularProgress,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  makeStyles,
+  Tooltip,
+  Typography
+} from '@material-ui/core';
 import ChessService from '../../services/ChessService';
 import { useServiceCall } from '../../utils/hooks';
+import { Help } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  list: {
+    width: '60%',
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: '1rem'
+  }
+}));
 
 const GamesList: React.FC = () => {
+  const classes = useStyles();
+
   const memoizedCall = React.useMemo(() => ChessService.getGames(true), []);
   const { loading, response: games = [], error } = useServiceCall(memoizedCall);
 
   return (
     <>
       {loading && <CircularProgress />}
-      {games.map(game => (
-        <Typography key={`game-item-${game.id}`}>Game Id: {game.uuid}</Typography>
-      ))}
+      <List className={classes.list}>
+        {games.map(game => (
+          <ListItem data-testid="game-list-item" key={`game-item-${game.id}`}>
+            <ListItemIcon>
+              <Tooltip
+                title={
+                  game.whitePlayer
+                    ? `${game.whitePlayer.username} is playing white in this game`
+                    : 'Join as white'
+                }
+              >
+                <span>
+                  <IconButton disabled={!!game.whitePlayer}>
+                    <Help />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            </ListItemIcon>
+            <ListItemText primaryTypographyProps={{ align: 'center' }} primary={game.uuid} />
+            <ListItemSecondaryAction>
+              <Tooltip
+                title={
+                  game.blackPlayer
+                    ? `${game.blackPlayer.username} is playing black in this game`
+                    : 'Join as black'
+                }
+              >
+                <span>
+                  <IconButton disabled={!!game.blackPlayer}>
+                    <Help />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
       {error && <Typography>Could not load games: {error}</Typography>}
     </>
   );
