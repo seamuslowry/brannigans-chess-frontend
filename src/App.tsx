a23539479c3d85498342de874edc432b22ecba9d
Seamus Lowry <spl1owry3@gmail.com>
2020-09-26 17:19:15 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import GamesList from './games/GamesList/GamesList';
 
 const App = () => {
   return (
-    <Box m={1}>
-      <Box>
-        <Typography align="center">
-          In the game of chess, you can never let your adversary see your pieces.
-        </Typography>
-        <Typography align="center">-Zapp Brannigan</Typography>
+    <Router>
+      <Box m={1}>
+        <Box>
+          <Typography align="center">
+            In the game of chess, you can never let your adversary see your pieces.
+          </Typography>
+          <Typography align="center">-Zapp Brannigan</Typography>
+        </Box>
+        <Box pt={2} display="flex" justifyContent="center">
+          <Switch>
+            <Route path="/">
+              <GamesList />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Box>
       </Box>
-      <Box pt={2} display="flex" justifyContent="center">
-        <GamesList />
-      </Box>
-    </Box>
+    </Router>
   );
 };
 
