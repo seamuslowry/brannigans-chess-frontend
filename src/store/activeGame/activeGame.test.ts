301dec45ab882c08deacc42cbccba95f4e949eae
Seamus Lowry <spl1owry3@gmail.com>
2020-09-28 15:54:07 -0400

    add actions and thunks to get pieces

--- src/store/activeGame/activeGame.test.ts
+++ src/store/activeGame/activeGame.test.ts
@@ -1,20 +1,42 @@
-import { reducer, clickTile, selectTile } from './activeGame';
+import { reducer, clickTile, selectTile, setTile, getPieces } from './activeGame';
 import createMockStore from 'redux-mock-store';
-import { testStore } from '../../utils/testData';
+import { blackRook, testStore } from '../../utils/testData';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import thunk from 'redux-thunk';
 import { AppState } from '../store';
 import { ActionCreator } from 'redux';
 import { immutableUpdate } from '../../utils/arrayHelpers';
+import config from '../../config';
+import { Piece } from '../../services/ChessService';
+
+const server = setupServer(
+  rest.get(`${config.serviceUrl}/pieces/0`, (req, res, ctx) => {
+    return res(
+      ctx.json<Piece[]>([blackRook])
+    );
+  })
+);
 
 const mockStore = createMockStore<AppState, ActionCreator<any>>([thunk]);
 const mockedStore = mockStore(testStore);
 
 beforeEach(() => mockedStore.clearActions());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('selects a tile', () => {
   const result = reducer(undefined, selectTile(0, 0, true));
 
   expect(result.tiles[0][0].selected).toBeTruthy();
+});
+
+test('sets a tile', () => {
+  const result = reducer(undefined, setTile(0, 0, blackRook));
+
+  expect(result.tiles[0][0].color).toEqual(blackRook.color);
+  expect(result.tiles[0][0].type).toEqual(blackRook.type);
 });
 
 test('clicks an unselected tile', async () => {
@@ -28,11 +50,28 @@ test('clicks a selected tile', async () => {
     ...testStore,
     activeGame: {
       ...testStore.activeGame,
-      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 0, 'selected', true)
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 0, { selected: true })
     }
   });
 
   await selectedStore.dispatch(clickTile(0, 0));
 
   expect(selectedStore.getActions()).toContainEqual(selectTile(0, 0, false));
+});
+
+test('gets pieces', async () => {
+  await mockedStore.dispatch(getPieces(0));
+
+  expect(mockedStore.getActions()).toContainEqual(setTile(0, 0, blackRook));
+});
+
+test('handles an error when getting pieces', async () => {
+  server.use(
+    rest.get(`${config.serviceUrl}/pieces/0`, (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+  await mockedStore.dispatch(getPieces(0));
+
+  expect(mockedStore.getActions()).not.toContainEqual(setTile(0, 0, blackRook));
 });
--- src/store/activeGame/activeGame.ts
+++ src/store/activeGame/activeGame.ts
@@ -1,8 +1,9 @@
-import { PieceColor, PieceType } from '../../services/ChessService';
+import ChessService, { Piece, PieceColor, PieceType } from '../../services/ChessService';
 import { flatten, immutableUpdate } from '../../utils/arrayHelpers';
 import { ThunkResult } from '../store';
 
 export const SELECT_TILE = 'chess/activeGame/SELECT_TILE';
+export const SET_TILE = 'chess/activeGame/SET_TILE';
 
 interface TileInfo {
   color?: PieceColor;
@@ -36,7 +37,16 @@ interface SelectTile {
   };
 }
 
-type ActiveGameAction = SelectTile;
+interface SetTile {
+  type: typeof SET_TILE;
+  payload: {
+    row: number;
+    col: number;
+    piece?: Piece;
+  };
+}
+
+type ActiveGameAction = SelectTile | SetTile;
 
 export const reducer = (
   state: ActiveGameState = initialState,
@@ -46,13 +56,18 @@ export const reducer = (
     case SELECT_TILE:
       return {
         ...state,
-        tiles: immutableUpdate(
-          state.tiles,
-          action.payload.row,
-          action.payload.col,
-          'selected',
-          action.payload.selected
-        )
+        tiles: immutableUpdate(state.tiles, action.payload.row, action.payload.col, {
+          selected: action.payload.selected
+        })
+      };
+    case SET_TILE:
+      const setToPiece = action.payload.piece;
+      return {
+        ...state,
+        tiles: immutableUpdate(state.tiles, action.payload.row, action.payload.col, {
+          color: setToPiece && setToPiece.color,
+          type: setToPiece && setToPiece.type
+        })
       };
     default:
       return initialState;
@@ -69,6 +84,28 @@ export const clickTile = (row: number, col: number): ThunkResult<void> => (dispa
     dispatch(selectTile(row, col, true));
   }
 };
+
+export const getPieces = (gameId: number): ThunkResult<void> => async dispatch => {
+  return ChessService.getPieces(gameId, undefined, false)
+    .then(res => {
+      res.data.forEach(piece => {
+        dispatch(setTile(piece.positionRow, piece.positionCol, piece));
+      });
+    })
+    .catch(error => {
+      // TODO update to use better error framework
+      console.error('could not get pieces for game');
+    });
+};
+
+export const setTile = (row: number, col: number, piece?: Piece): SetTile => ({
+  type: SET_TILE,
+  payload: {
+    row,
+    col,
+    piece
+  }
+});
 
 export const selectTile = (row: number, col: number, selected: boolean): SelectTile => ({
   type: SELECT_TILE,
