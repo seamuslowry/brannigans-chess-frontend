a9336bd6124b9b3dc49089d8135cb5af7f964aaa
Seamus Lowry <spl1owry3@gmail.com>
2020-09-28 16:18:12 -0400

    add tile rendering for pieces

--- src/games/Tile/Tile.test.tsx
+++ src/games/Tile/Tile.test.tsx
@@ -3,11 +3,12 @@ import { fireEvent, render } from '@testing-library/react';
 import Tile from './Tile';
 import { Provider } from 'react-redux';
 import createMockStore from 'redux-mock-store';
-import { testStore } from '../../utils/testData';
+import { blackRook, makePiece, testStore } from '../../utils/testData';
 import { SELECT_TILE } from '../../store/activeGame/activeGame';
 import { ActionCreator } from 'redux';
 import thunk from 'redux-thunk';
 import { AppState } from '../../store/store';
+import { immutableUpdate } from '../../utils/arrayHelpers';
 
 const mockStore = createMockStore<AppState, ActionCreator<any>>([thunk]);
 const mockedStore = mockStore(testStore);
@@ -29,4 +30,144 @@ test('selects on click', async () => {
       type: SELECT_TILE
     })
   );
+});
+
+test('renders a black knight', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 1, makePiece('KNIGHT', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={0} col={1} />
+    </Provider>
+  );
+
+  const tile = getByText('H');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a black bishop', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 2, makePiece('BISHOP', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={0} col={2} />
+    </Provider>
+  );
+
+  const tile = getByText('B');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a black queen', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 3, makePiece('QUEEN', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={0} col={3} />
+    </Provider>
+  );
+
+  const tile = getByText('Q');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a black king', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 4, makePiece('KING', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={0} col={4} />
+    </Provider>
+  );
+
+  const tile = getByText('K');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a black pawn', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 1, 0, makePiece('PAWN', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={1} col={0} />
+    </Provider>
+  );
+
+  const tile = getByText('P');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a black rook', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 0, 0, makePiece('ROOK', 'BLACK'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={0} col={0} />
+    </Provider>
+  );
+
+  const tile = getByText('R');
+
+  expect(tile).toBeInTheDocument();
+});
+
+test('renders a white pawn', async () => {
+  const pieceStore = mockStore({
+    ...testStore,
+    activeGame: {
+      ...testStore,
+      tiles: immutableUpdate(testStore.activeGame.tiles, 6, 0, makePiece('PAWN', 'WHITE'))
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={pieceStore}>
+      <Tile row={6} col={0} />
+    </Provider>
+  );
+
+  const tile = getByText('P');
+
+  expect(tile).toBeInTheDocument();
 });
--- src/games/Tile/Tile.tsx
+++ src/games/Tile/Tile.tsx
@@ -1,6 +1,7 @@
-import { Box, Theme, useTheme } from '@material-ui/core';
+import { Box, Theme, Typography, useTheme } from '@material-ui/core';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { PieceColor, PieceType } from '../../services/ChessService';
 import { clickTile } from '../../store/activeGame/activeGame';
 import { AppState } from '../../store/store';
 
@@ -13,10 +14,42 @@ const getBgColor = (theme: Theme, row: number, col: number, selected: boolean) =
   if (selected) {
     return theme.palette.action.selected;
   } else if ((row + col) % 2 === 0) {
-    return theme.palette.primary.main;
-  } else {
     return theme.palette.secondary.light;
+  } else {
+    return theme.palette.primary.main;
   }
+};
+
+const getPiece = (type?: PieceType, color?: PieceColor) => {
+  if (!(type && color)) return null;
+
+  let letter;
+  switch (type) {
+    case 'BISHOP':
+      letter = 'B';
+      break;
+    case 'PAWN':
+      letter = 'P';
+      break;
+    case 'QUEEN':
+      letter = 'Q';
+      break;
+    case 'KING':
+      letter = 'K';
+      break;
+    case 'KNIGHT':
+      letter = 'H';
+      break;
+    case 'ROOK':
+      letter = 'R';
+      break;
+  }
+
+  return (
+    <Typography align="center" color={color === 'WHITE' ? 'textPrimary' : 'error'}>
+      {letter}
+    </Typography>
+  );
 };
 
 const Tile: React.FC<Props> = ({ row, col }) => {
@@ -26,8 +59,15 @@ const Tile: React.FC<Props> = ({ row, col }) => {
   const selected = useSelector<AppState, boolean>(
     state => state.activeGame.tiles[row][col].selected
   );
+  const pieceColor = useSelector<AppState, PieceColor | undefined>(
+    state => state.activeGame.tiles[row][col].color
+  );
+  const pieceType = useSelector<AppState, PieceType | undefined>(
+    state => state.activeGame.tiles[row][col].type
+  );
 
   const bgColor = getBgColor(theme, row, col, selected);
+  const piece = getPiece(pieceType, pieceColor);
 
   const handleClick = () => {
     dispatch(clickTile(row, col));
@@ -40,7 +80,9 @@ const Tile: React.FC<Props> = ({ row, col }) => {
       gridColumn={col + 1}
       bgcolor={bgColor}
       onClick={handleClick}
-    />
+    >
+      {piece}
+    </Box>
   );
 };
 
