3ce3e4f05fe054b251daeff0dea23656f1d04316
Seamus Lowry <spl1owry3@gmail.com>
2020-09-29 15:22:21 -0400

    add taken pieces to active game

--- src/games/ActiveGame/ActiveGame.test.tsx
+++ src/games/ActiveGame/ActiveGame.test.tsx
@@ -3,15 +3,29 @@ import { render } from '@testing-library/react';
 import ActiveGame from './ActiveGame';
 import createMockStore from 'redux-mock-store';
 import { testStore } from '../../utils/testData';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { Provider } from 'react-redux';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { CLEAR_GAME, SET_GAME_ID } from '../../store/activeGame/activeGame';
 import thunk from 'redux-thunk';
+import config from '../../config';
+import { Piece } from '../../services/ChessService';
 
 const mockStore = createMockStore([thunk]);
 const mockedStore = mockStore(testStore);
 
 beforeEach(() => mockedStore.clearActions());
+
+const server = setupServer(
+  rest.get(`${config.serviceUrl}/pieces/0`, (req, res, ctx) => {
+    return res(ctx.json<Piece[]>([]));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('sets and unsets the game id', async () => {
   const { container, unmount } = render(
--- src/games/ActiveGame/ActiveGame.tsx
+++ src/games/ActiveGame/ActiveGame.tsx
@@ -1,8 +1,10 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { clearGame, setGameId } from '../../store/activeGame/activeGame';
 import Board from '../Board/Board';
+import TakenPieces from '../TakenPieces/TakenPieces';
 
 interface ExpectedRouteParams {
   id?: string;
@@ -22,7 +24,19 @@ const ActiveGame: React.FC = () => {
     };
   }, [gameId, dispatch]);
 
-  return <Board gameId={gameId} />;
+  return (
+    <Box display="grid" gridTemplateColumns="1fr min-content 1fr">
+      <Box gridRow={1} gridColumn={1}>
+        <TakenPieces gameId={gameId} color="WHITE" />
+      </Box>
+      <Box gridRow={1} gridColumn={2}>
+        <Board gameId={gameId} />
+      </Box>
+      <Box gridRow={1} gridColumn={3}>
+        <TakenPieces gameId={gameId} color="BLACK" />
+      </Box>
+    </Box>
+  );
 };
 
 export default ActiveGame;
