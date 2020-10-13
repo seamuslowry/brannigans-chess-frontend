1f526dcfbddead5cabc456eb51d818e68f101390
Seamus Lowry <spl1owry3@gmail.com>
2020-10-13 16:31:05 -0400

    add socket data to redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,25 @@
 import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk, { ThunkAction, ThunkMiddleware } from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as activeGameReducer, ActiveGameState } from './activeGame/activeGame';
 import { reducer as notificationsReducer, NotificationsState } from './notifications/notifications';
+import { reducer as socketsReducer, SocketsState } from './sockets/sockets';
 
 export type ThunkResult<R> = ThunkAction<R, AppState, undefined, AnyAction>;
 
 export interface AppState {
   activeGame: ActiveGameState;
   notifications: NotificationsState;
+  sockets: SocketsState;
 }
 
 const rootReducer = combineReducers<AppState>({
   activeGame: activeGameReducer,
-  notifications: notificationsReducer
+  notifications: notificationsReducer,
+  sockets: socketsReducer
 });
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(thunk as ThunkMiddleware<AppState, AnyAction>)
+  composeWithDevTools(applyMiddleware(thunk as ThunkMiddleware<AppState, AnyAction>))
 );
