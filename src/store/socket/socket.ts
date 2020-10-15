3b90ed755e6f37c92d13bde53529817c8416910a
Seamus Lowry <spl1owry3@gmail.com>
2020-10-15 16:16:42 -0400

    refactor redux socket behavior

--- src/store/socket/socket.ts
+++ src/store/socket/socket.ts
@@ -1,55 +1,47 @@
-import Stomp from 'stompjs';
+import {
+  StompConnected,
+  StompDisconnected,
+  StompMessage,
+  STOMP_CLOSED,
+  STOMP_CONNECTED,
+  STOMP_MESSAGE
+} from '../middleware/stomp';
 
-export const SET_SOCKET = 'chess/sockets/SET_SOCKET';
-export const REMOVE_SOCKET = 'chess/sockets/REMOVE_SOCKET';
+export interface Message {
+  topic: string;
+  data: string;
+}
 
 export interface SocketState {
-  current: Stomp.Client | null;
+  messages: Message[];
+  connected: boolean;
 }
 
 export const initialState: SocketState = {
-  current: null
+  messages: [],
+  connected: false
 };
 
-interface SetSocket {
-  type: typeof SET_SOCKET;
-  payload: Stomp.Client;
-}
-
-interface RemoveSocket {
-  type: typeof REMOVE_SOCKET;
-}
-
-type SocketAction = SetSocket | RemoveSocket;
+type SocketAction = StompConnected | StompDisconnected | StompMessage;
 
 export const reducer = (state: SocketState = initialState, action: SocketAction): SocketState => {
-  let currentSocket;
-
   switch (action.type) {
-    case SET_SOCKET:
-      currentSocket = state.current;
-      currentSocket && currentSocket.disconnect(() => {});
+    case STOMP_CONNECTED:
       return {
         ...state,
-        current: action.payload
+        connected: true
       };
-    case REMOVE_SOCKET:
-      currentSocket = state.current;
-      currentSocket && currentSocket.disconnect(() => {});
+    case STOMP_CLOSED:
       return {
         ...state,
-        current: null
+        connected: false
+      };
+    case STOMP_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, action.payload]
       };
     default:
       return state;
   }
 };
-
-export const setSocket = (socket: Stomp.Client): SetSocket => ({
-  type: SET_SOCKET,
-  payload: socket
-});
-
-export const removeSocket = (): RemoveSocket => ({
-  type: REMOVE_SOCKET
-});
