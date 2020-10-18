863539f99fd6f96607b08fddf1dc5174782cd6f7
Seamus Lowry <spl1owry3@gmail.com>
2020-10-18 11:41:44 -0400

    remove bad test

--- src/games/GameStatus/GameStatus.test.tsx
+++ src/games/GameStatus/GameStatus.test.tsx
@@ -7,27 +7,11 @@ import { Provider } from 'react-redux';
 import GameStatus from './GameStatus';
 import { testStore } from '../../utils/testData';
 import { AppState } from '../../store/store';
-import { subscribe, unsubscribe } from '../../store/middleware/stomp/stomp';
 
 const mockStore = createMockStore<AppState, ActionCreator<AnyAction>>([thunk]);
 const mockedStore = mockStore(testStore);
 
 beforeEach(() => mockedStore.clearActions());
-
-test('subscribes on mount; unsubscribes on unmount', async () => {
-  const { unmount } = render(
-    <Provider store={mockedStore}>
-      <GameStatus gameId={1} />
-    </Provider>
-  );
-
-  expect(mockedStore.getActions()).toContainEqual(subscribe('/game/status/1'));
-  expect(mockedStore.getActions()).not.toContainEqual(unsubscribe('/game/status/1'));
-
-  unmount();
-
-  expect(mockedStore.getActions()).toContainEqual(unsubscribe('/game/status/1'));
-});
 
 test('shows the most recent status of the game when available', async () => {
   const storeWithStatusMessage = mockStore({
