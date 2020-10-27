b58cabefb3f1808836d5d8073824e8dd3de12abb
Seamus Lowry <spl1owry3@gmail.com>
2020-10-27 12:26:42 -0400

    remove unnecessary

--- src/auth/AuthenticationDialog/AuthenticationDialog.test.tsx
+++ src/auth/AuthenticationDialog/AuthenticationDialog.test.tsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import createMockStore from 'redux-mock-store';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import thunk from 'redux-thunk';
-import { testStore, whiteMove } from '../../utils/testData';
-import { Move } from '../../services/ChessService.types';
-import config from '../../config';
+import { testStore } from '../../utils/testData';
 import AuthenticationDialog from './AuthenticationDialog';
-import { SET_TILE } from '../../store/activeGame/activeGame';
-import { immutableUpdate } from '../../utils/arrayHelpers';
-import { SEND_ALERT } from '../../store/notifications/notifications';
 
 const mockStore = createMockStore([thunk]);
-const mockedStore = mockStore({
-  ...testStore,
-  activeGame: {
-    ...testStore.activeGame
-  }
-});
-
-const server = setupServer(
-  rest.post(`${config.serviceUrl}/pieces/promote/QUEEN`, (req, res, ctx) => {
-    return res(
-      ctx.json<Move[]>([whiteMove])
-    );
-  })
-);
-
-beforeEach(() => mockedStore.clearActions());
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+const mockedStore = mockStore(testStore);
 
 test('opens as signup', async () => {
   const { getByText } = render(
