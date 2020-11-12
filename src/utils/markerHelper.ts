1c9263cad5292e5cb43f4725bee59161634d3559
Seamus Lowry <spl1owry3@gmail.com>
2020-11-12 16:34:40 -0500

    add marker helper

---
+++ src/utils/markerHelper.ts
@@ -0,0 +1,5 @@
+const rows = [8, 7, 6, 5, 4, 3, 2, 1];
+const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+export const displayRow = (n: number) => rows[n];
+export const displayCol = (n: number) => cols[n];
