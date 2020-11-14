1253870175a3f9d5934464df3448e55291243a7a
Seamus Lowry <spl1owry3@gmail.com>
2020-11-13 22:09:40 -0500

    revert jwt fix

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
 import config from '../config';
 
 export const chessApi = axios.create({
@@ -11,12 +10,6 @@ chessApi.interceptors.request.use(axiosConfig => {
   const token = localStorage.getItem('token');
 
   if (!token) return axiosConfig;
-
-  const claims = jwtDecode(token) as { exp: number };
-  if (claims.exp * 1000 < Date.now()) {
-    localStorage.removeItem('token');
-    return axiosConfig;
-  }
 
   return {
     ...axiosConfig,
