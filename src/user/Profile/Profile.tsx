1f5ecd0230b7475160e9e8a241298e2998a9ffa0
Seamus Lowry <spl1owry3@gmail.com>
2020-11-15 19:45:59 -0500

    coming soon component

---
+++ src/user/Profile/Profile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Profile: React.FC = () => {
+  return <Typography align="center">Coming Soon</Typography>;
+};
+
+export default Profile;
