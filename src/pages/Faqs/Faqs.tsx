952d3579872efb7e234f3591cc3060f71a8390c6
Seamus Lowry <spl1owry3@gmail.com>
2020-11-15 20:58:44 -0500

    actual commit

---
+++ src/pages/Faqs/Faqs.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import Faq from '../../molecules/Faq/Faq';
+
+const faqs = [
+  {
+    question: 'Why can I only see half the pieces?',
+    answer:
+      "As the inspirational quote states: \" you can never let your adversary see your pieces\". You can't see your adversary's pieces and they can't see yours."
+  },
+  {
+    question: 'Are there plans to allow a draw?',
+    answer: 'Coward.'
+  }
+];
+
+const Faqs: React.FC = () => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" width="100%">
+      {faqs.map((q, index) => (
+        <Box key={`faq-${index}`} p={1} width="80%">
+          <Faq {...q} />
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default Faqs;
