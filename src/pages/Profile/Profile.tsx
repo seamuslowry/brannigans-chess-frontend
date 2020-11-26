5cfd6dabd12a637f6741435635dc55bca29eded9
Seamus Lowry <spl1owry3@gmail.com>
2020-11-26 16:50:13 -0500

    add placeholder for player stats

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,6 +5,10 @@ import { useSelector } from 'react-redux';
 import { Player } from '../../services/ChessService.types';
 import { AppState } from '../../store/store';
 import PlayerGames from '../../organisms/PlayerGames/PlayerGames';
+import DataGroup from '../../molecules/DataGroup/DataGroup';
+import DataLabel from '../../atoms/DataLabel/DataLabel';
+import DataValue from '../../atoms/DataValue/DataValue';
+import Data from '../../molecules/Data/Data';
 
 const Profile: React.FC = () => {
   const player = useSelector<AppState, Player | undefined>(state => state.auth.player);
@@ -15,6 +19,68 @@ const Profile: React.FC = () => {
     <Grid item container xs={9} spacing={2}>
       <Grid item xs={12} md={3}>
         <DisplayName player={player} />
+        <DataGroup my={1}>
+          <Data>
+            <DataLabel>TOTAL GAMES</DataLabel>
+            <DataValue>200</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>TOTAL WINS</DataLabel>
+            <DataValue>100</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WIN PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>TOTAL DRAWS</DataLabel>
+            <DataValue>50</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>DRAW PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WHITE GAMES</DataLabel>
+            <DataValue>100</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WHITE WINS</DataLabel>
+            <DataValue>50</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WHITE WIN PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WHITE DRAWS</DataLabel>
+            <DataValue>25</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>WHITE DRAW PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>BLACK GAMES</DataLabel>
+            <DataValue>100</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>BLACK WINS</DataLabel>
+            <DataValue>50</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>BLACK WIN PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>BLACK DRAWS</DataLabel>
+            <DataValue>25</DataValue>
+          </Data>
+          <Data>
+            <DataLabel>BLACK DRAW PERCENTAGE</DataLabel>
+            <DataValue>50%</DataValue>
+          </Data>
+        </DataGroup>
       </Grid>
       <Grid item xs={12} md={9}>
         <PlayerGames player={player} />
