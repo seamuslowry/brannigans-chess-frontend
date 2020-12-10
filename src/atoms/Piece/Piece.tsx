8f58d05860c3b8569cd3795649fbd548388db3a5
Seamus Lowry <spl1owry3@gmail.com>
2020-12-10 16:24:03 -0500

    make changes i'll need to revert every time i debug

--- src/atoms/Piece/Piece.tsx
+++ src/atoms/Piece/Piece.tsx
@@ -9,18 +9,16 @@ interface Props {
 }
 
 const Piece = React.forwardRef<HTMLImageElement, Props & React.ImgHTMLAttributes<HTMLImageElement>>(
-  ({ type, color, height = '100%', width = '100%', ...rest }, ref) => {
-    return (
-      <img
-        ref={ref}
-        alt={`${color}-${type}`}
-        height={height}
-        width={width}
-        src={`/pieces/${color}_${type}.svg`}
-        {...rest}
-      />
-    );
-  }
+  ({ type, color, height = '100%', width = '100%', ...rest }, ref) => (
+    <img
+      ref={ref}
+      alt={`${color}-${type}`}
+      height={height}
+      width={width}
+      src={`/pieces/${color}_${type}.svg`}
+      {...rest}
+    />
+  )
 );
 
 export default Piece;
