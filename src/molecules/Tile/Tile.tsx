b60f7c7b09c5408193b324d3174d8d2c5a4467e3
Seamus Lowry <spl1owry3@gmail.com>
2020-12-19 15:51:00 -0500

    render tile backgrounds

--- src/molecules/Tile/Tile.tsx
+++ src/molecules/Tile/Tile.tsx
@@ -1,9 +1,9 @@
-import { Box, Theme, useTheme } from '@material-ui/core';
+import { Box, darken, Theme, useTheme } from '@material-ui/core';
 import React from 'react';
 import { useDrop } from 'react-dnd';
 import { useSelector } from 'react-redux';
 import { Piece as PieceEntity } from '../../services/ChessService.types';
-import { dragMove } from '../../store/boards/boards';
+import { dragMove, makeGetDestination, makeGetSource } from '../../store/boards/boards';
 import { makeGetActivePiece } from '../../store/pieces/pieces';
 import { AppState, useAppDispatch } from '../../store/store';
 import DraggablePiece from '../DraggablePiece/DraggablePiece';
@@ -15,9 +15,24 @@ interface Props {
   disabled?: boolean;
 }
 
-const getBgColor = (theme: Theme, row: number, col: number, hovering: boolean) => {
+const getBgColor = (
+  theme: Theme,
+  row: number,
+  col: number,
+  states: {
+    hovering?: boolean;
+    source?: boolean;
+    destination?: boolean;
+  }
+) => {
+  const { hovering, source, destination } = states;
+
   if (hovering) {
     return theme.palette.action.selected;
+  } else if (source) {
+    return darken(theme.palette.action.active, 0.3);
+  } else if (destination) {
+    return darken(theme.palette.action.active, 0.5);
   } else if ((row + col) % 2 === 0) {
     return theme.palette.secondary.light;
   } else {
@@ -45,12 +60,27 @@ const Tile: React.FC<Props> = ({ row, col, gameId, disabled }) => {
   });
 
   const getActivePiece = React.useMemo(makeGetActivePiece, []);
-
-  const piece = useSelector<AppState, PieceEntity | undefined>(state =>
-    getActivePiece(state.pieces, gameId, { row, col })
+  const getSourceTile = React.useMemo(makeGetSource, []);
+  const getDestinationTile = React.useMemo(makeGetDestination, []);
+  const position = React.useMemo(
+    () => ({
+      row,
+      col
+    }),
+    [row, col]
   );
 
-  const bgColor = getBgColor(theme, row, col, isOver);
+  const piece = useSelector<AppState, PieceEntity | undefined>(state =>
+    getActivePiece(state.pieces, gameId, position)
+  );
+  const source = useSelector<AppState, boolean>(state =>
+    getSourceTile(state.boards, gameId, position)
+  );
+  const destination = useSelector<AppState, boolean>(state =>
+    getDestinationTile(state.boards, gameId, position)
+  );
+
+  const bgColor = getBgColor(theme, row, col, { hovering: isOver, source, destination });
 
   return (
     <Box
--- src/store/boards/boards.ts
+++ src/store/boards/boards.ts
@@ -1,4 +1,10 @@
-import { AnyAction, createAsyncThunk, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import {
+  AnyAction,
+  createAsyncThunk,
+  createEntityAdapter,
+  createSelector,
+  createSlice
+} from '@reduxjs/toolkit';
 import ChessService from '../../services/ChessService';
 import { Game, Move, Piece } from '../../services/ChessService.types';
 import { GAME_STATUS_PREFIX } from '../games/games';
@@ -81,5 +87,25 @@ const boardSlice = createSlice({
 export const { addBoard } = boardSlice.actions;
 
 export const { selectById: selectBoardById } = boardAdapter.getSelectors();
+
+export const makeGetSource = () =>
+  createSelector(
+    selectBoardById,
+    (_, __, position: TilePosition) => position,
+    (board, givenPosition) =>
+      !!board?.move &&
+      board?.move.srcRow === givenPosition.row &&
+      board?.move.srcCol === givenPosition.col
+  );
+
+export const makeGetDestination = () =>
+  createSelector(
+    selectBoardById,
+    (_, __, position: TilePosition) => position,
+    (board, givenPosition) =>
+      !!board?.move &&
+      board?.move.dstRow === givenPosition.row &&
+      board?.move.dstCol === givenPosition.col
+  );
 
 export default boardSlice.reducer;
